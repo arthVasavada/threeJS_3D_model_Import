4d657c8c64c3c85c78d5b668ed780273c7e7724e
thetransientone <arth.vasavada@gmail.com>
2022-01-26 15:16:09 +0530

    commit

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ const sizes ={
     height: window.innerHeight
 }
 
-
-
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
+camera.getWorldPosition.set(0,1,2)
+scene.add(camera)
 
 const renderer = new THREE.WebGL1Renderer({
-
+    canvas: canvas
 })
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
+renderer.updateShadowMap.enabled = true
+renderer.gammaOutput = true
