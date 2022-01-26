72793faa9373f4d77a99d65f4e69efc84bb64fd0
thetransientone <arth.vasavada@gmail.com>
2022-01-26 18:32:56 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# threeJS_3D_model_Import
-Basic threeJS: Importing 3D model for webGL
---
+++ index.js
@@ -0,0 +1,63 @@
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js"
+import {GLTFLoader} from "https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js"
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+const loader = new GLTFLoader()
+loader.load("./assets/wraith.glb", function(glb){
+console.log(glb)
+const root = glb.scene
+root.scale.set(0.03,0.03,0.03)
+scene.add(root);
+})
+
+const light = new THREE.DirectionalLight(0xffffff,1)
+//light.rotateOnWorldAxis()
+scene.add(light)
+
+/*
+// Defining Geometry and material 
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+})
+
+// Applying material to the mesh 
+const boxmesh = new THREE.Mesh(geometry,material)
+scene.add(boxmesh)
+*/
+
+//Boiler Plate Code
+
+//Defining Sizes according to window sizes
+const sizes= {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+//Defining perspective camera for renderer
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,.1,100)
+camera.position.set(0,1,2)
+scene.add(camera)
+
+//Defining type renderer
+const renderer = new THREE.WebGL1Renderer({
+   canvas: canvas
+})
+
+//Defining renderer properties
+renderer.setSize(sizes.width,sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
+renderer.shadowMap.enabled = true
+renderer.gammaOutput = true
+
+//Calling render method for rendering
+
+let animate = () => {
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+}
+
+animate()
