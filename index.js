8e5c1db17bf2d1ab6a5a41493d8ba0b0edd68e7c
thetransientone <arth.vasavada@gmail.com>
2022-01-26 14:55:14 +0530

    commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import * as THREE from "./three.js-master/build/three.module.js"
 import {GLTFLoader} from "./three.js-master/examples/jsm/loaders/GLTFLoader.js"
 
+const canvas = document.querySelector(".webgl")
+const scene = new THREE.Scene()
+
+//Boiler Plate Code
+const sizes ={
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+
+
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
+
+const renderer = new THREE.WebGL1Renderer({
+
+})
